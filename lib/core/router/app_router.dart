609199980f6c954b59076bc8811b0752ded14bc9
moeez5251 <moeez4241@gmail.com>
2026-05-15 18:44:50 +0500

    Remove unused imports and clean up pubspec.yaml assets section

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:serviq/features/input/presentation/screens/input_screen.dart';
