1daaa4debd2dcd35c2335d01eeaa50856886603f
moeez5251 <moeez4241@gmail.com>
2026-05-16 13:40:48 +0500

    Add missing import for Material package in app_router.dart

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:serviq/features/input/presentation/screens/input_screen.dart';
